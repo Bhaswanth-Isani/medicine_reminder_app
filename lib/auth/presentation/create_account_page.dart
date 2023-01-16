1a813c4c5871f6e5e421a6f9e3ed600ebb17b49b
Bhaswanth Isani <bhaswanthisani@gmail.com>
2023-01-16 12:27:48 +0530

    Implemented CreateAccountPage fully

--- lib/auth/presentation/create_account_page.dart
+++ lib/auth/presentation/create_account_page.dart
@@ -1,24 +1,113 @@
-import 'dart:developer';
-
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:medicine_reminder_app/auth/application/auth_repository.dart';
+import 'package:medicine_reminder_app/core/presentation/router.dart';
 
-class CreateAccountPage extends ConsumerWidget {
+class CreateAccountPage extends HookConsumerWidget {
   const CreateAccountPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final nameController = useTextEditingController();
+    final emailController = useTextEditingController();
+    final passwordController = useTextEditingController();
+    final isLoading = ref.watch(authRepositoryProvider).isLoading;
+
+    final formKey = GlobalKey<FormState>();
+
+    ref.listen(authRepositoryProvider, (previous, next) {
+      if (next.error != null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(next.error.toString()),
+            behavior: SnackBarBehavior.floating,
+            backgroundColor: Theme.of(context).errorColor,
+          ),
+        );
+      } else if (next.user != null) {
+        nameController.clear();
+        emailController.clear();
+        passwordController.clear();
+
+        context.replaceRoute(const MedicineRoute());
+      }
+    });
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Create Account'),
-      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24),
         child: Form(
+          key: formKey,
           child: Column(
-            children: const [
-              CustomTextField(),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomTextField(
+                isLoading: isLoading,
+                label: 'NAME',
+                controller: nameController,
+                validator: (value) {
+                  if (value == null || value.length < 3) {
+                    return 'Enter a valid name';
+                  }
+                  return null;
+                },
+              ),
+              CustomTextField(
+                isLoading: isLoading,
+                label: 'E-MAIL',
+                controller: emailController,
+                validator: (value) {
+                  if (value == null ||
+                      !RegExp(r"^[a-zA-Z\d.a-zA-Z!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z\d]+\.[a-zA-Z]+")
+                          .hasMatch(value)) {
+                    return 'Enter a valid E-Mail';
+                  }
+                  return null;
+                },
+              ),
+              CustomTextField(
+                isLoading: isLoading,
+                label: 'PASSWORD',
+                controller: passwordController,
+                validator: (value) {
+                  if (value == null || value.length < 6) {
+                    return 'Password should have more than 6 characters';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: ElevatedButton(
+                  onPressed: isLoading
+                      ? null
+                      : () {
+                          if (formKey.currentState?.validate() ?? false) {
+                            ref
+                                .read(authRepositoryProvider.notifier)
+                                .createAccountUsingEmailAndPassword(
+                                  email: emailController.text,
+                                  password: passwordController.text,
+                                  name: nameController.text,
+                                );
+                          }
+                        },
+                  child: isLoading
+                      ? const CircularProgressIndicator()
+                      : const Text(
+                          'Create Account',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                ),
+              ),
+              TextButton(
+                onPressed: () => context.replaceRoute(const LoginRoute()),
+                child: const Text('Login'),
+              )
             ],
           ),
         ),
@@ -28,12 +117,42 @@ class CreateAccountPage extends ConsumerWidget {
 }
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  const CustomTextField({
+    super.key,
+    required this.label,
+    required this.controller,
+    required this.validator,
+    required this.isLoading,
+  });
+
+  final String label;
+  final TextEditingController controller;
+  final String? Function(String?)? validator;
+  final bool isLoading;
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 12,
+          ),
+        ),
+        TextFormField(
+          enabled: !isLoading,
+          validator: validator,
+          controller: controller,
+          // ignore: avoid_bool_literals_in_conditional_expressions
+          obscureText: label == 'PASSWORD' ? true : false,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/medicine/presentation/medicine.dart
+++ lib/medicine/presentation/medicine.dart
@@ -1,13 +1,27 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:feather_icons/feather_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:medicine_reminder_app/auth/application/auth_repository.dart';
+import 'package:medicine_reminder_app/core/presentation/router.dart';
 
-class MedicinePage extends StatelessWidget {
+class MedicinePage extends ConsumerWidget {
   const MedicinePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Medicine'),
+      ),
+      body: Center(
+        child: IconButton(
+          onPressed: () {
+            context.replaceRoute(const LoginRoute());
+            ref.read(authRepositoryProvider.notifier).signOut();
+          },
+          icon: const Icon(FeatherIcons.logOut),
+        ),
       ),
     );
   }
